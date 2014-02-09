316a62b15837edef039ac9a1932f081e31e08b98
Bill Boyer <william.boyer.nyc@gmail.com>
2014-02-09 13:06:00 -0500

    Improved empty-result handling, with unfortunately a ton of white-space changes thrown in too.

--- src/js/facts.js
+++ src/js/facts.js
@@ -1,77 +1,86 @@
 var Facts = Facts || {};
 
 Facts.Util = {
-  loadJSON: function(url, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    };
-    xhr.open("GET", url, true);
-    xhr.send();
-  },
+	loadJSON: function(url, success, error) {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function()
+		{
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {
+					if (success)
+						success(JSON.parse(xhr.responseText));
+				} else {
+					if (error)
+						error(xhr);
+				}
+			}
+		};
+		xhr.open("GET", url, true);
+		xhr.send();
+	},
   
-  colors: [ "lightpink", "lightblue", "lightgreen", "lightgoldenrodyellow", "lightcyan", "lightcoral", "lightseagreen" ],
-  _nextColor: 0,
+	colors: [ "lightpink", "lightblue", "lightgreen", "lightgoldenrodyellow", "lightcyan", "lightcoral", "lightseagreen" ],
+	_nextColor: 0,
   
-  nextColor: function() {
-    Facts.Util._nextColor %= Facts.Util.colors.length;
-    return Facts.Util.colors[Facts.Util._nextColor++];
-  },
+	nextColor: function() {
+		Facts.Util._nextColor %= Facts.Util.colors.length;
+		return Facts.Util.colors[Facts.Util._nextColor++];
+	},
   
-  resetColor: function() {
-    Facts.Util._nextColor = 0;
-  }
+	resetColor: function() {
+		Facts.Util._nextColor = 0;
+	}
 };
 
 Facts.Search = {
-  renderResults: function(div, result, types, forceBgColor, containerClass) {
-    var type = types[0];
-    var values = result[type].values;
-  
-    if (!values.length)
-      return;
+	renderResults: function(div, result, types, forceBgColor, containerClass) {
+		var type = types[0];
+		if (!result[type])
+			return;
 
-    var containerDiv = document.createElement("div");
-    containerDiv.setAttribute("class", "fact-result-container " + containerClass  + "-" + type);
-    div.appendChild(containerDiv);
+		var values = result[type].values;
   
-    for (var i in values) {
-      var childDiv = document.createElement("div");
-      childDiv.setAttribute("class", "fact-node fact-result");
+		var containerDiv = document.createElement("div");
+		containerDiv.setAttribute("class", "fact-result-container " + containerClass  + "-" + type);
+		var hasChildren = false;
 
-      if ((i >= 1) || forceBgColor)
-        childDiv.setAttribute("style", "background-color: " + Facts.Util.nextColor());
+		for (var i in values) {
+			var value = values[i];
+			if (forceBgColor && !value.children)
+				continue;
+			else
+				hasChildren = true;
+
+			var childDiv = document.createElement("div");
+			childDiv.setAttribute("class", "fact-node fact-result");
+
+			if ((i >= 1) || forceBgColor)
+				childDiv.setAttribute("style", "background-color: " + Facts.Util.nextColor());
   
-      childDiv.textContent = values[i].label + " (" + (Math.floor(Math.random() * 1000) % 10) + ")";
+			childDiv.textContent = value.label + " (" + (Math.floor(Math.random() * 1000) % 10) + ")";
   
-      containerDiv.appendChild(childDiv);
+			containerDiv.appendChild(childDiv);
   
-      if ((types.length > 1) && (values[i].children))
-        Facts.Search.renderResults(childDiv, values[i].children, types.slice(1), false, containerClass);
-    }
-  },
+			if ((types.length > 1) && (value.children))
+				Facts.Search.renderResults(childDiv, value.children, types.slice(1), false, containerClass);
+		}
+
+		if (hasChildren)
+			div.appendChild(containerDiv);
+	},
   
-  search: function(url, subjDiv, objDiv) {
-    Facts.Util.loadJSON(url,
-      function(data) {
-        subjDiv.innerHTML = "";
-        Facts.Util.resetColor();        
-        Facts.Search.renderResults(subjDiv, data, ["subj", "pred", "obj"], true, "fact-right-container");
+	search: function(url, subjDiv, objDiv) {
+		Facts.Util.loadJSON(url,
+							function(data) {
+								subjDiv.innerHTML = "";
+								Facts.Util.resetColor();        
+								Facts.Search.renderResults(subjDiv, data, ["subj", "pred", "obj"], true, "fact-right-container");
   
-        objDiv.innerHTML = "";
-        Facts.Util.resetColor();        
-        Facts.Search.renderResults(objDiv, data, ["obj", "pred", "subj"], true, "fact-left-container");
-      },
-      function(xhr) { console.error(xhr); }
-    );
-  }
+								objDiv.innerHTML = "";
+								Facts.Util.resetColor();        
+								Facts.Search.renderResults(objDiv, data, ["obj", "pred", "subj"], true, "fact-left-container");
+							},
+							function(xhr) { console.error(xhr); }
+							);
+	}
 };
