6ad1c9b247ec5af09c3bab9640d6a26243704457
Bill Boyer <william.boyer.nyc@gmail.com>
2014-03-27 13:22:19 -0400

    More isolation from site's surrounding styling.  Also moved the demo-generation script into this project.

---
+++ src/js/facts-demo.js
@@ -0,0 +1,56 @@
+var Facts = Facts || {};
+
+/*jshint multistr: true */
+
+Facts.Demo = {
+	init: function(div) {
+        jQuery(document).ready(function() {
+            if (document.createStyleSheet)
+                document.createStyleSheet('/css/facts.css');
+            else
+                $("head").append($("<link rel='stylesheet' href='/css/facts.css' type='text/css' media='screen' />"));
+
+			jQuery(div).html(' \
+				<div class="row"> \
+					<div class="col-xs-12 col-sm-4">   \
+						<div class="fact-search-subj"> \
+							<div class="fact-input-label">Subj:</div><input class="fact-input" id="fact-pfx-subj" type="text" onkeyup="Facts.Demo.search(\'subj\');" onfocus="Facts.Demo.search(\'subj\');" /> \
+						</div> \
+					</div> \
+					<div class="col-xs-12 col-sm-4 col-sm-offset-4"> \
+						<div class="fact-search-obj"> \
+							<div class="fact-input-label">Obj:</div><input class="fact-input" id="fact-pfx-obj" type="text" onkeyup="Facts.Demo.search(\'obj\');" onfocus="Facts.Demo.search(\'obj\'); "/> \
+						</div> \
+					</div> \
+				</div> \
+				<div class="row"> \
+					<div class="col-xs-12"> \
+						<div id="fact-top-results" class="fact-results"></div> \
+						<div id="fact-bottom-results" class="fact-results"></div> \
+					</div> \
+				</div> \
+			');
+		});
+	},
+
+	search: function(node) {
+		var i = document.getElementById('fact-pfx-' + node);
+		q = i.value;
+		if (q === '')
+		return;
+
+		var t = document.getElementById('fact-top-results');
+		var b = document.getElementById('fact-bottom-results');
+
+		if (node === 'subj') {
+			document.getElementById('fact-pfx-obj').classList.remove('active');
+			Facts.Search.search('/api/facts/browse/subj?q=' + q, t, b, 'Subjects matching: "' + q + '"', "Those Subjects as Objects");
+		}
+		else {
+			document.getElementById('fact-pfx-subj').classList.remove('active');
+			Facts.Search.search('/api/facts/browse/obj?q=' + q, b, t, "Those Objects as Subjects", 'Objects matching: "' + q + '"');
+		}
+
+		i.classList.add('active');
+	}
+};
